08d3e34515aa0a82654a47a1cf11175871e2898c
SamKirkland <github@samkirkland.com>
2017-10-16 23:03:15 -0500

    Initial Checkin

---
+++ CustomProtocol/CustomProtocol.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+
+namespace CustomProtocol
+{
+    class CustomProtocol
+    {
+        public delegate void URI_Handle(Uri toHandle);
+        public URI_Handle uriHandler;
+        private string registryKey;
+        private string customURI;
+        
+        /// <summary>
+        /// Create a new CustomProtocol attached to the uri specified
+        /// </summary>
+        /// <param name="uri">The uri you with to attach to. Cannot contain spaces</param>
+        /// <exception cref="InvalidOperationException">The custom uri cannot contain spaces</exception>
+        public CustomProtocol(string protocolURI, URI_Handle uriCallback) // ToDo: add log location, and bind location as params
+        {
+            if (protocolURI.Contains(' '))
+            {
+                throw new InvalidOperationException("Custom protocol cannot contain spaces");
+            }
+            
+            customURI = protocolURI;
+            registryKey = $"URL:{customURI} Protocol";
+            uriHandler = uriCallback;
+        }
+
+        public bool isAttached() {
+            using (RegistryKey hkcrClass = Registry.ClassesRoot.OpenSubKey(customURI))
+            {
+                if (hkcrClass != null)
+                {
+                    string val = hkcrClass.GetValue("").ToString();
+                    if (val == null)
+                    {
+                        Console.WriteLine("value not found");
+                        return false;
+                    }
+                    else
+                    {
+                        // use the value
+                        return true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("key not found");
+                    return false;
+                }
+            }
+        }
+
+        /// <exception cref="Exception">attach was already called (and was successful) use .isAttached to see attach status</exception>
+        public void attach()
+        {
+            if (isAttached())
+            {
+                return; // Custom protocol already attached
+            }
+
+            string appPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+
+            // HKEY_CLASSES_ROOT\myscheme
+            using (RegistryKey hkcrClass = Registry.ClassesRoot.CreateSubKey(customURI))
+            {
+                hkcrClass.SetValue(null, registryKey);
+                hkcrClass.SetValue("URL Protocol", String.Empty, RegistryValueKind.String);
+
+                // use the application's icon as the URI scheme icon
+                using (RegistryKey defaultIcon = hkcrClass.CreateSubKey("DefaultIcon"))
+                {
+                    string iconValue = String.Format("\"{0}\",0", appPath);
+                    defaultIcon.SetValue(null, iconValue);
+                }
+
+                // open the application and pass the URI to the command-line
+                using (RegistryKey shell = hkcrClass.CreateSubKey("shell"))
+                {
+                    using (RegistryKey open = shell.CreateSubKey("open"))
+                    {
+                        using (RegistryKey command = open.CreateSubKey("command"))
+                        {
+                            string cmdValue = String.Format("\"{0}\" \"%1\"", appPath);
+                            command.SetValue(null, cmdValue);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void detact()
+        {
+            if (!isAttached())
+            {
+                return; // Custom protocol isn't attached
+            }
+
+            Registry.ClassesRoot.DeleteSubKeyTree(customURI);
+        }
+    }
+}
---
+++ CustomProtocol/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace CustomProtocol
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+
+        private static string getTFPath() {
+            var possibleTFSPaths = new List<string> {
+                "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\",
+                "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer",
+                "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer",
+                "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer"
+            };
+            
+            return getPath("VS140COMNTOOLS", possibleTFSPaths, "TF.exe");
+        }
+        private static string getTFPTPath()
+        {
+            var possibleTFSPTPaths = new List<string> {
+                "C:\\Program Files (x86)\\Microsoft Team Foundation Server 2015 Power Tools\\"
+            };
+
+            return getPath("VS140COMNTOOLS", possibleTFSPTPaths, "TFPT.exe");
+        }
+
+        /// <summary>
+        /// Returns a path based on the current users env vars (and system vars)
+        /// </summary>
+        /// <param name="envPath">env/system var to extract</param>
+        /// <param name="backupPath">Path to use if the env/system var isn't set</param>
+        /// <param name="fileName">The file name to look for</param>
+        /// <returns>the path stored in the env/system var, or the backupPath</returns>
+        private static string getPath(string envPath, List<string> backupPath, string fileName) {
+            try {
+                Environment.CurrentDirectory = Environment.GetEnvironmentVariable(envPath);
+                DirectoryInfo info = new DirectoryInfo(".");
+                return $"{info.FullName}\\..\\IDE\\{fileName}";
+            }
+            catch (ArgumentNullException e) {
+                // user doesn't have the environment var, return the hard-coded path
+                foreach (var path in backupPath) {
+                    if (File.Exists(path + fileName))
+                    {
+                        return path + fileName;
+                    }
+                    else
+                    {
+                        log.Warn($"Could not find file envPath:{envPath}, backup:{backupPath}, exception:{e.ToString()}");
+                        Console.WriteLine($"Could not find file: {path}{fileName}");
+                    }
+                }
+                
+            }
+            catch (Exception e) {
+                log.Fatal($"getPath exception! envPath:{envPath}, backup:{backupPath}, exception:{e.ToString()}");
+                Console.WriteLine($"getPath exception! envPath:{envPath}, backup:{backupPath}, exception:{e.ToString()}");
+            }
+
+            log.Warn($"Failed to find {fileName}, to fix this error create a environment variable 'VS140COMNTOOLS' that points to the FOLDER with {fileName}");
+            Console.WriteLine($"Failed to find {fileName}, to fix this error create a environment variable 'VS140COMNTOOLS' that points to the FOLDER with {fileName}");
+            return null;
+        }
+
+
+        static void tf_uriReceived(Uri fullURI)
+        {
+            string uriParams = fullURI.LocalPath.ToString();
+            log.Info($"Command tfmerge. Running:{getTFPath()} {uriParams}");
+            Process.Start(getTFPath(), uriParams);
+            // ToDo: add .StandardOutput.ReadToEnd(); and verify the process ran
+        }
+
+
+        static void tfpt_uriReceived(Uri fullURI)
+        {
+            string uriParams = fullURI.LocalPath.ToString();
+            log.Info($"Command tfpt. Running:{getTFPath()} {uriParams}");
+            Process.Start(getTFPTPath(), uriParams);
+            // ToDo: add .StandardOutput.ReadToEnd(); and verify the process ran
+        }
+
+        static void error(string message) {
+            log.Fatal(message);
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            // custom uri handlers
+            CustomProtocol tfCommand = new CustomProtocol("tfmerge", tf_uriReceived);
+            CustomProtocol tfptCommand = new CustomProtocol("tfpt", tfpt_uriReceived);
+            
+            if (args.Length > 0)
+            { // a URI was passed and needs to be handled
+                log.Info("App trigger from uri");
+
+                try
+                {
+                    Console.WriteLine("Running URI");
+                    Uri command = new Uri(args[0].Trim());
+
+                    if (command.Scheme == "tfmerge")
+                    {
+                        tfCommand.uriHandler(command);
+                    }
+                    else
+                    {
+                        tfptCommand.uriHandler(command);
+                    }
+
+                }
+                catch (UriFormatException e)
+                {
+                    log.Fatal($"Invalid uri {e}");
+                    Console.WriteLine("Invalid URI.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                log.Info("Custom protocol started from the user");
+                Console.WriteLine("Installing custom URI's");
+
+                try
+                {
+                    if (tfCommand.isAttached())
+                    {
+                        Console.WriteLine("TF command already found, uninstalling first.");
+                        tfCommand.detact();
+                    }
+                    tfCommand.attach();
+                    Console.WriteLine("Successfully installed tfmerge uri.");
+                }
+                catch (Exception e) {
+                    error($"Failed to install tfmerge protocol. Error:{e.Message}");
+                }
+                try
+                {
+                    if (tfptCommand.isAttached())
+                    {
+                        Console.WriteLine("TFPT command already found, uninstalling first.");
+                        tfptCommand.detact();
+                    }
+                    tfptCommand.attach();
+                    Console.WriteLine("Successfully installed tfpt uri.");
+                }
+                catch (Exception e)
+                {
+                    error($"Failed to install tfpt protocol. Error:{e.Message}");
+                }
+
+                Console.WriteLine("Install Done. Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
