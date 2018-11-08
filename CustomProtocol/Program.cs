eb229415cdb7331df9c3e7b7c5a28e2b82c8e644
Sam Kirkland <SamKirkland@users.noreply.github.com>
2018-11-08 15:10:04 -0600

    Remove Path finding
    Removed the code to find the path, instead you need to specify it in your environment vars

--- CustomProtocol/Program.cs
+++ CustomProtocol/Program.cs
@@ -8,82 +8,15 @@ namespace CustomProtocol
     class Program
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
-
-        private static string getTFPath() {
-            var possibleTFSPaths = new List<string> {
-                "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\",
-                "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer",
-                "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer",
-                "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer"
-            };
-            
-            return getPath("VS140COMNTOOLS", possibleTFSPaths, "TF.exe");
-        }
-        private static string getTFPTPath()
-        {
-            var possibleTFSPTPaths = new List<string> {
-                "C:\\Program Files (x86)\\Microsoft Team Foundation Server 2015 Power Tools\\"
-            };
-
-            return getPath("VS140COMNTOOLS", possibleTFSPTPaths, "TFPT.exe");
-        }
-
-        /// <summary>
-        /// Returns a path based on the current users env vars (and system vars)
-        /// </summary>
-        /// <param name="envPath">env/system var to extract</param>
-        /// <param name="backupPath">Path to use if the env/system var isn't set</param>
-        /// <param name="fileName">The file name to look for</param>
-        /// <returns>the path stored in the env/system var, or the backupPath</returns>
-        private static string getPath(string envPath, List<string> backupPath, string fileName) {
-            try {
-                Environment.CurrentDirectory = Environment.GetEnvironmentVariable(envPath);
-                DirectoryInfo info = new DirectoryInfo(".");
-                return $"{info.FullName}\\..\\IDE\\{fileName}";
-            }
-            catch (ArgumentNullException e) {
-                // user doesn't have the environment var, return the hard-coded path
-                foreach (var path in backupPath) {
-                    if (File.Exists(path + fileName))
-                    {
-                        return path + fileName;
-                    }
-                    else
-                    {
-                        log.Warn($"Could not find file envPath:{envPath}, backup:{backupPath}, exception:{e.ToString()}");
-                        Console.WriteLine($"Could not find file: {path}{fileName}");
-                    }
-                }
-                
-            }
-            catch (Exception e) {
-                log.Fatal($"getPath exception! envPath:{envPath}, backup:{backupPath}, exception:{e.ToString()}");
-                Console.WriteLine($"getPath exception! envPath:{envPath}, backup:{backupPath}, exception:{e.ToString()}");
-            }
-
-            log.Warn($"Failed to find {fileName}, to fix this error create a environment variable 'VS140COMNTOOLS' that points to the FOLDER with {fileName}");
-            Console.WriteLine($"Failed to find {fileName}, to fix this error create a environment variable 'VS140COMNTOOLS' that points to the FOLDER with {fileName}");
-            return null;
-        }
-
-
+        
         static void tf_uriReceived(Uri fullURI)
         {
             string uriParams = fullURI.LocalPath.ToString();
-            log.Info($"Command tfmerge. Running:{getTFPath()} {uriParams}");
-            Process.Start(getTFPath(), uriParams);
-            // ToDo: add .StandardOutput.ReadToEnd(); and verify the process ran
+
+            // execute merge command and close "/C" (/K Carries out the command specified by string but remains)
+            Process.Start("cmd.exe", $"/C echo Merging Changes... & TF {uriParams}");
         }
-
-
-        static void tfpt_uriReceived(Uri fullURI)
-        {
-            string uriParams = fullURI.LocalPath.ToString();
-            log.Info($"Command tfpt. Running:{getTFPath()} {uriParams}");
-            Process.Start(getTFPTPath(), uriParams);
-            // ToDo: add .StandardOutput.ReadToEnd(); and verify the process ran
-        }
-
+        
         static void error(string message) {
             log.Fatal(message);
             Console.WriteLine(message);
@@ -93,7 +26,6 @@ namespace CustomProtocol
         {
             // custom uri handlers
             CustomProtocol tfCommand = new CustomProtocol("tfmerge", tf_uriReceived);
-            CustomProtocol tfptCommand = new CustomProtocol("tfpt", tfpt_uriReceived);
             
             if (args.Length > 0)
             { // a URI was passed and needs to be handled
@@ -108,11 +40,6 @@ namespace CustomProtocol
                     {
                         tfCommand.uriHandler(command);
                     }
-                    else
-                    {
-                        tfptCommand.uriHandler(command);
-                    }
-
                 }
                 catch (UriFormatException e)
                 {
@@ -139,20 +66,6 @@ namespace CustomProtocol
                 }
                 catch (Exception e) {
                     error($"Failed to install tfmerge protocol. Error:{e.Message}");
-                }
-                try
-                {
-                    if (tfptCommand.isAttached())
-                    {
-                        Console.WriteLine("TFPT command already found, uninstalling first.");
-                        tfptCommand.detact();
-                    }
-                    tfptCommand.attach();
-                    Console.WriteLine("Successfully installed tfpt uri.");
-                }
-                catch (Exception e)
-                {
-                    error($"Failed to install tfpt protocol. Error:{e.Message}");
                 }
 
                 Console.WriteLine("Install Done. Press any key to exit.");
